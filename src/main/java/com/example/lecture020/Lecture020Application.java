65cafaff88742950154e8468dfc0fd28a4bf43be
henry <hyeongjong90@gmail.com>
2024-11-04 15:16:29 +0900

    First commit

---
+++ src/main/java/com/example/lecture020/Lecture020Application.java
@@ -0,0 +1,13 @@
+package com.example.lecture020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lecture020Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lecture020Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lecture020/Lecture020ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lecture020;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lecture020ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
